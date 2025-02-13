dd83798609803e9fd022b16b3a0974c48dbce372
lokeshsn08 <54617033+lokeshsn08@users.noreply.github.com>
2025-02-13 01:54:29 -0700

    Update StringCalculator.rb
    find strings calculation

--- StringCalculator.rb
+++ StringCalculator.rb
@@ -1,16 +1,22 @@
-class IncubyteStringCalculator
-   def remove_special_characters(input)
-     cleaned_string = input.gsub(/[^0-9]/, ' ')
-     #clean input string from special characters
-     integer_array = cleaned_string.split.map(&:to_i)
-    #converts each elemt of array into intgers values
-      negatives = integer_array.select { |num| num.to_i < 0 }
-      raise "Negative numbers not allowed: #{negatives.join(', ')}" unless negatives.empty?
-      # escape negative values and return sum
-      
-      integer_array.map(&:to_i).sum
-   end
-end
+class SimpleCalculator
+  def add(number_string)
 
-strinCalc = IncubyteStringCalculator.new
-puts strinCalc.remove_special_characters("78,33")   # Output: 6
+   separator = ","
+
+    number_string = number_string.gsub("\n", separator) # Replace newlines with the separator
+    number_list = number_string.split(separator)
+    
+    total_sum = 0
+    negative_numbers = []
+
+    number_list.each do |num|
+      number = num.to_i
+      if number < 0
+        negative_numbers << number
+      else
+        total_sum += number
+      end
+    end
+    return total_sum
+  end
+end
